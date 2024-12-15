4cf904ef6669372dd3932bf39523dd6a6e2cc4fb
mertyildiz68 <mertyildiz2060@gmail.com>
2024-12-15 11:30:14 +0100

    Update map.js

--- deelopdracht 3/js/map.js
+++ deelopdracht 3/js/map.js
@@ -1,12 +1,8 @@
-
-let map = L.map('apMap').setView([51.22516, 4.42191], 16);
-
+let map = L.map('apMap').setView([51.2379014, 4.4391804], 16);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
-let bounds = [[51.2379014,4.4391804], [51.2379014,4.4391804]];
-
-let apMarker = L.marker([51.2379014,4.4391804]).addTo(map);
-apMarker.bindPopup("<b>Gasthuishoevestraat 80</b><br>Antwerp, Belgium").openPopup();
+let bounds = [[51.2379014, 4.4391804], [51.2379014, 4.4391804]];
+let apMarker = L.marker([51.2379014, 4.4391804]).addTo(map);
+apMarker.bindPopup("<b>Gasthuishoevestraat 80</b><br>2170 Antwerp, Belgium").openPopup();

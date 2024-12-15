98721da70f96751804aebfe48bbf728bae42f95a
mertyildiz68 <mertyildiz2060@gmail.com>
2024-12-15 11:26:08 +0100

    Update map.js

--- deelopdracht 3/js/map.js
+++ deelopdracht 3/js/map.js
@@ -6,7 +6,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let bounds = [[51.22538, 4.42110], [51.22494, 4.42210]];
+let bounds = [[51.2379014,4.4391804], [51.2379014,4.4391804]];
 
-let apMarker = L.marker([51.22516, 4.42191]).addTo(map);
+let apMarker = L.marker([51.2379014,4.4391804]).addTo(map);
 apMarker.bindPopup("<b>Gasthuishoevestraat 80</b><br>Antwerp, Belgium").openPopup();
